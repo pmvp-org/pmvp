be1f84ccc60c4cb155e57c56211c34c9f2d59778
Aubrey Goodman <aubrey.goodman@gmail.com>
2019-05-26 21:57:23 -0700

    Expanded converter interface

--- PMVP/Converter.swift
+++ PMVP/Converter.swift
@@ -11,7 +11,11 @@ open class Converter<K: Hashable, T: AbstractObject, P: Proxy<K>> {
 
 	public init() {}
 
-	open func fromProxy(_ proxy: P) -> T {
+	open func insert(using proxy: P) -> T {
+		fatalError("unimplemented \(#function)")
+	}
+
+	open func update(_ object: T, from proxy: P) {
 		fatalError("unimplemented \(#function)")
 	}
 
