e09518bb5546881a44be05e85e58512d372d911a
Aubrey Goodman <aubrey.goodman@gmail.com>
2019-05-27 08:43:50 -0700

    Use more purposeful method name

--- PMVP/Converter.swift
+++ PMVP/Converter.swift
@@ -11,11 +11,7 @@ open class Converter<K: Hashable, T: AbstractObject, P: Proxy<K>> {
 
 	public init() {}
 
-	open func insert(using proxy: P) -> T {
-		fatalError("unimplemented \(#function)")
-	}
-
-	open func update(_ object: T, from proxy: P) {
+	open func copyInto(_ object: T, from proxy: P) {
 		fatalError("unimplemented \(#function)")
 	}
 
